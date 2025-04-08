0301b8a175385e00389b0435d61ee425c5e7bcf2
Sarmad Abbasi <sarmadsuper3@gmail.com>
2025-04-08 14:27:32 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Home from './pages/Home';
@@ -7,18 +7,28 @@ import Services from './pages/Services';
 import AboutUs from "./pages/AboutUs";
 import Contact from './pages/Contact';
 
+// ScrollToTop component to handle scroll restoration
+function ScrollToTop() {
+  const { pathname } = useLocation();
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  
+  return null;
+}
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-white">
+        <ScrollToTop />
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/services" element={<Services />} />
           <Route path="/contact" element={<Contact />} />
-          <Route path="/AboutUs" element={<AboutUs />} />
-          
+          <Route path="/aboutUs" element={<AboutUs />} />
         </Routes>
         <Footer />
       </div>
